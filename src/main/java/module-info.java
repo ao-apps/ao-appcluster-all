3062bcf9aae2be3c62471aaf9367b351353ed154
AO Industries, Inc <support@aoindustries.com>
2022-04-20 04:48:39 -0500

    Significant manual code formatting clean-up…
    … in preparation for an automatic conversion to Google Java Style Guide:
    
    1) Added braces around if/else/for/while where missing
    
    2) Indentation is now 2-space, all file formats
    
      a) HTML indentation changed from tab to 2-space
    
      b) XML indentation changed from 4-space to 2-space
    
    3) Continuation is 4-space for JSON, Java, and JavaScript
    
    4) Updated NetBeans, kwrite, and kate settings

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 /*
  * ao-appcluster-all - Convenience POM to include all application-level clustering tools.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -21,10 +21,10 @@
  * along with ao-appcluster-all.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.aoapps.appcluster.all {
-	// Direct
-	requires transitive com.aoapps.appcluster.core; // <groupId>com.aoapps</groupId><artifactId>ao-appcluster-core</artifactId>
-	requires transitive com.aoapps.appcluster.csync2; // <groupId>com.aoapps</groupId><artifactId>ao-appcluster-csync2</artifactId>
-	requires transitive com.aoapps.appcluster.dnsonly; // <groupId>com.aoapps</groupId><artifactId>ao-appcluster-dnsonly</artifactId>
-	requires transitive com.aoapps.appcluster.imap; // <groupId>com.aoapps</groupId><artifactId>ao-appcluster-imap</artifactId>
-	requires transitive com.aoapps.appcluster.jdbc; // <groupId>com.aoapps</groupId><artifactId>ao-appcluster-jdbc</artifactId>
+  // Direct
+  requires transitive com.aoapps.appcluster.core; // <groupId>com.aoapps</groupId><artifactId>ao-appcluster-core</artifactId>
+  requires transitive com.aoapps.appcluster.csync2; // <groupId>com.aoapps</groupId><artifactId>ao-appcluster-csync2</artifactId>
+  requires transitive com.aoapps.appcluster.dnsonly; // <groupId>com.aoapps</groupId><artifactId>ao-appcluster-dnsonly</artifactId>
+  requires transitive com.aoapps.appcluster.imap; // <groupId>com.aoapps</groupId><artifactId>ao-appcluster-imap</artifactId>
+  requires transitive com.aoapps.appcluster.jdbc; // <groupId>com.aoapps</groupId><artifactId>ao-appcluster-jdbc</artifactId>
 }
